3437687b69d0cf3aabfcdf7faf73aa766399c89a
Giulia Elisa Pereira <111393483+GiuElisa@users.noreply.github.com>
2023-09-20 19:23:51 -0300

    Add files via upload

---
+++ Projeto_3Bim_IGC/Projeto_3Bim_IGC/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Projeto_3Bim_IGC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(65, 283);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(384, 226);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gerar Imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(147, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(235, 212);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.Location = new System.Drawing.Point(575, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(384, 226);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.Location = new System.Drawing.Point(575, 274);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(384, 226);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.Location = new System.Drawing.Point(575, 506);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(384, 226);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 775);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Projeto_3Bim_IGC/Projeto_3Bim_IGC/Form1.cs
@@ -0,0 +1,170 @@
+﻿/*Colegio Técnico Antônio Teixeira Fernandes (Univap)
+ *Curso Técnico em Informática - Data de Entrega: 30 / 08 / 2023
+* Autores do Projeto: Giovana Ferreira Fonseca
+*                     Giulia Elisa Pereira
+*
+* Turma: 3H
+* Projeto 3º Bim
+ * Observação: < colocar se houver>
+ * 
+ * Projeto_3Bim_IGC.cs
+ * ******************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_3Bim_IGC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int coluna = 0;
+        int linha = 0;
+        int i, j;
+        double K;
+        Color cor;
+        Bitmap img, panela, imgNova, imgCinza, imgBin;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("C:\\Imagens\\Imagem_A.jpg");
+            pictureBox2.Load("C:\\Imagens\\Imagem_B.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tirarFundo();
+            GerarImagem();
+            GrayLevel();
+            Binario();
+            pictureBox1.Image = imgNova;
+            pictureBox2.Image = panela;
+        }
+        public Color botaCor(int a, int r, int g, int b)
+        {
+            Color cor = Color.FromArgb(a, r, g, b);
+            return cor;
+        }
+
+        Bitmap tirarFundo()
+        {
+            img = new Bitmap("C:\\Imagens\\Imagem_B.jpg");
+            coluna = img.Width;
+            linha = img.Height;
+            panela = new Bitmap(coluna, linha);
+
+            for (int i = 0; i <= coluna - 1; i++)
+            {
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    int r = img.GetPixel(i, j).R;
+                    int g = img.GetPixel(i, j).G;
+                    int b = img.GetPixel(i, j).B;
+
+
+                    //1º tira o amarelo, média do r,g      2º reflexo da panela, média do r,g,b
+                    if ((r + g) / 2 >= 220 && (r + g + b) / 3 <= 230)
+                    {
+                        cor = botaCor(0,0,0,0);
+                    }
+                    else
+                    {
+                        cor = botaCor(255, r, g, b);
+                        panela.SetPixel(i, j, cor);
+                    }
+                }
+            }
+            return panela;
+        }
+
+        Bitmap GerarImagem()
+        {
+            imgNova = new Bitmap("C:\\Imagens\\Imagem_A.jpg");
+            i = 140;
+
+            for (int y = 0; y < coluna; y++)
+            {
+                i++;
+                j = 0;
+                for (int x = 0; x < linha; x++)
+                {
+                    j++;
+                    Color pixels = panela.GetPixel(y, x);
+                    if (pixels.A != 0)
+                    {
+                        imgNova.SetPixel(i, j, pixels);
+                    }
+                }
+            }
+            pictureBox3.Image = imgNova;
+            imgNova.Save("C:\\Imagens\\Imagem_C.jpg");
+            return imgNova;
+        }
+
+        public void GrayLevel()
+        {
+            img = imgNova;
+            coluna = img.Width;
+            linha = img.Height;
+            imgCinza = new Bitmap(coluna, linha);
+            for (int i = 0; i <= coluna - 1; i++)
+            {
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    double r = img.GetPixel(i, j).R;
+                    double g = img.GetPixel(i, j).G;
+                    double b = img.GetPixel(i, j).B;
+
+                    K = r * 0.3 + g * 0.59 + b * 0.11;
+
+                    cor = botaCor(255, (int)K, (int)K, (int)K);
+                    imgCinza.SetPixel(i, j, cor);
+
+                }
+            }
+            imgCinza.Save("C:\\Imagens\\Cinza.jpg");
+            pictureBox4.Image = imgCinza;
+        }
+
+        public void Binario()
+        {
+            img = imgNova;
+            coluna = img.Width; 
+            linha = img.Height; 
+            imgBin = new Bitmap(coluna, linha);
+
+            for (int i = 0; i <= coluna - 1; i++)
+            {
+                for (int j = 0; j <= linha - 1; j++)
+                {
+                    double r = img.GetPixel(i, j).R;
+                    double g = img.GetPixel(i, j).G;
+                    double b = img.GetPixel(i, j).B;
+
+                    K = r * 0.3 + g * 0.59 + b * 0.11;
+
+                    if (K >= 127)
+                        K = 255;
+                    else
+                        K = 0;
+
+                    cor = botaCor(255, (int)K, (int)K, (int)K);
+                    imgBin.SetPixel(i, j, cor);
+
+                }
+            }
+            imgBin.Save("C:\\Imagens\\Binaria.jpg");
+            pictureBox5.Image = imgBin;
+        }
+    }
+}
